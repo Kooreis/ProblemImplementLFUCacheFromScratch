ef1f4f7c13934d6f28ce71be2cd7d939ad192771
Kooreis <kooreis.official@gmail.com>
2024-01-05 17:35:20 -0600

    This update introduces the DLinkedList class, which is a doubly linked list that will be used to store nodes in the cache. It includes methods for inserting and removing nodes, removing the last node, and checking if the list is empty.

--- solution.cs
+++ solution.cs
@@ -1,8 +1,35 @@
-class Node {
-    constructor(key, value) {
-        this.key = key;
-        this.value = value;
-        this.freq = 0;
-        this.prev = this.next = null;
+class DLinkedList {
+    constructor() {
+        this.head = new Node(null, null);
+        this.tail = new Node(null, null);
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+    }
+
+    insert(node) {
+        node.next = this.head.next;
+        node.prev = this.head;
+        this.head.next.prev = node;
+        this.head.next = node;
+    }
+
+    remove(node) {
+        let prev = node.prev;
+        let next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    removeLast() {
+        if (this.head.next !== this.tail) {
+            let last = this.tail.prev;
+            this.remove(last);
+            return last.key;
+        }
+        return -1;
+    }
+
+    isEmpty() {
+        return this.head.next === this.tail;
     }
 }
